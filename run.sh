7519039765ae5807346ffd45c0cea4feebd57d40
Maximiliano Osorio <maxiosorio@gmail.com>
2024-05-28 15:43:47 -0400

    fix: improve readme

--- run.sh
+++ run.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-echo "Your command line args (appArgs) are: $@"
-
 Greeting=$1
 Target=$2
 
 FULL_GREETING="${Greeting} ${Target}. My name is ${_tapisJobOwner}"
 echo "$FULL_GREETING"
-echo `pwd`
-echo  ${_tapisJobWorkingDir}
-
-fileToModify=$_tapisExecSystemInputDir/in.txt
 
 echo $FULL_GREETING > $_tapisExecSystemOutputDir/out.txt
